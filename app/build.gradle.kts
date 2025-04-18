37f26ca8102c45ce5afde282e930c740b11a82d0
Sean P. McBride <seanpmcb@gmail.com>
2025-04-18 04:35:16 -0400

    Workout customizations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,12 @@ android {
     buildFeatures {
         compose = true
     }
+    
+    sourceSets {
+        getByName("main") {
+            assets.srcDirs("src/main/assets")
+        }
+    }
 }
 
 dependencies {
